846191f1f44eaa595157fa975e2f69670dd8b3f9
xinshen z <zcy@ieigen.com>
2022-07-27 20:49:43 +0800

    init

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,16 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+// <header className="App-header">
+//   <p>
+//     Edit <code>src/App.js</code> and save to reload.
+//   </p>
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Learn React
+//   </a>
+// </header>
