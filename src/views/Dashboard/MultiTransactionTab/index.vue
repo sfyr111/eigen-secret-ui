c8aa2f114f63d72e21e36e8c072d948f159b19e7
qiaomengnan <qiaomengnan@ypshengxian.com>
2023-05-18 08:24:29 +0800

    取消余额展示先存为0

--- src/views/Dashboard/MultiTransactionTab/index.vue
+++ src/views/Dashboard/MultiTransactionTab/index.vue
@@ -50,7 +50,7 @@ export default {
         if (res.errno == 0) {
           this.assetsInfos = res.data.map(item => {
             return {
-              rightVal: '$0', //item.assetId,
+              rightVal: item.assetId, //'$0',
               tokenName: item.tokenInfo?.symbol,
               leftDes: null,
               icon: item.tokenInfo?.logoURI,
