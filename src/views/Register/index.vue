85d40a637b15ad1f2e402fe2ab1b937f51de131b
qiaomengnan <qiaomengnan@ypshengxian.com>
2023-05-24 15:06:24 +0800

    无用代码删除

--- src/views/Register/index.vue
+++ src/views/Register/index.vue
@@ -20,29 +20,8 @@
 
 <script>
 
-import {createSecretAccount} from "@/contractUtils/account";
 import AlertDialog from '@/components/AlertDialog/index';
-import {connectMetaMask, doConnectMetaMask} from "@/contractUtils/metaMask";
-import {getSecretAccount, getSigner, getAddress, getSecretManager} from "@/store";
-import { ethers } from 'ethers';
-
-async function loadScriptFromBlob(blob) {
-  return new Promise((resolve, reject) => {
-    const script = document.createElement('script');
-    const url = URL.createObjectURL(blob);
-    script.src = url;
-    script.type = 'text/javascript';
-    script.onload = () => {
-      URL.revokeObjectURL(url);
-      resolve(window.wc); // Assuming the script sets a global variable named 'wc'
-    };
-    script.onerror = () => {
-      URL.revokeObjectURL(url);
-      reject(new Error('Failed to load the script'));
-    };
-    document.head.appendChild(script);
-  });
-}
+import {doConnectMetaMask} from "@/contractUtils/metaMask";
 
 export default {
   name: 'register-page',
@@ -75,45 +54,9 @@ export default {
         this.showAlert(data.message, 2)
       }
     },
-    async createAccount() {
-      const batchproof = await createSecretAccount('abu')
-      console.log(batchproof)
-    },
-    async createImport() {
-      let wcUrl = `http://localhost:3000/public/main_update_state_js/witness_calculator.js`;
-      console.log("Fetching witness_calculator.js from:", wcUrl);
-
-      const response = await window.fetch(wcUrl);
-      const wcContent = await response.text();
-
-      const wcContentModified = wcContent.replace(/module\.exports\s*=/, 'window.wc =');
-
-      const wcBlob = new Blob([wcContentModified], {type: 'text/javascript'});
-      console.log("Created Blob with witness_calculator.js content:", wcBlob);
-
-      try {
-        await loadScriptFromBlob(wcBlob);
-        const wc = window.wc;
-        console.log("Imported witness_calculator.js, default export:", wc);
-      } catch (err) {
-        console.error('Error loading script:', err);
-      }
-    },
     metamaskLogin() {
-      // console.log(Prover)
-      // let secretSDK = new SecretSDK('')
-      // console.log(secretSDK)
-      // secretSDK.createAccount();
       this.$emit('login-end', 1)
-      // this.$router.push('/dashboard')
     },
-  },
-
-  async mounted() {
-  },
-
-  created() {
-    console.log('token')
   },
 
 };
