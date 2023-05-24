c43df9b3fc84c327de3b9f66ea461fea10d671b2
qiaomengnan <qiaomengnan@ypshengxian.com>
2023-05-24 15:08:02 +0800

    无用代码删除

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -26,30 +26,11 @@
 
 <script>
 
-import {createSecretAccount} from "@/contractUtils/account";
 import {connectMetaMask} from "@/contractUtils/metaMask";
 import secretManager from '@/SecretManager/SecretManager';
 import AlertDialog from '@/components/AlertDialog/index';
-import { getSigner, setSigner, setAddress, setAlias, setSdk, getAlias } from "@/store";
-import { __DEFAULT_ALIAS__ } from "@eigen-secret/core/dist-browser/utils";
-
-async function loadScriptFromBlob(blob) {
-  return new Promise((resolve, reject) => {
-    const script = document.createElement('script');
-    const url = URL.createObjectURL(blob);
-    script.src = url;
-    script.type = 'text/javascript';
-    script.onload = () => {
-      URL.revokeObjectURL(url);
-      resolve(window.wc); // Assuming the script sets a global variable named 'wc'
-    };
-    script.onerror = () => {
-      URL.revokeObjectURL(url);
-      reject(new Error('Failed to load the script'));
-    };
-    document.head.appendChild(script);
-  });
-}
+import {getAlias, getSigner, setAlias, setSdk, setSigner} from "@/store";
+import {__DEFAULT_ALIAS__} from "@eigen-secret/core/dist-browser/utils";
 
 export default {
   name: 'login-page',
@@ -98,8 +79,6 @@ export default {
           console.log('login res data ', res)
           setSdk(res.data)
           setAlias(res.data.alias)
-          // // init signer
-          // secretManager.initSignature({alias: __DEFAULT_ALIAS__, user: getSigner()})
           this.$router.push('/dashboard')
         } else {
           this.showAlert(res.message, 2)
@@ -114,65 +93,14 @@ export default {
       }).finally(() => {
         eloading.close()
       })
-      // try {
-      //   await secretManager.initSDK({ alias: 'EIGEN_BUILTIN_PLACEHOLDER', user: this.user });
-      //   this.$emit('create-end')
-      // } catch (e) {
-      //   console.error(e)
-      // } finally {
-      //   eloading.close()
-      // }
     },
     toLogin() {
       this.$router.push('/LoginStep')
     },
-    async connect() {
-      // await connectMetaMask();
-      // let secretManager = getSecretManager();
-      // await secretManager.initSDK({alias: 'EIGEN_BUILTIN_PLACEHOLDER', password: '12356', user: getSigner()})
-      //   this.$router.push('/dashboard')
-      //} else {
-      // todo 弹出error
-      //}
-
-    },
-    async createAccount() {
-      const batchproof = await createSecretAccount('abu')
-      console.log(batchproof)
-    },
-    async createImport() {
-      let wcUrl = `http://localhost:3000/public/main_update_state_js/witness_calculator.js`;
-      console.log("Fetching witness_calculator.js from:", wcUrl);
-
-      const response = await window.fetch(wcUrl);
-      const wcContent = await response.text();
-
-      const wcContentModified = wcContent.replace(/module\.exports\s*=/, 'window.wc =');
-
-      const wcBlob = new Blob([wcContentModified], {type: 'text/javascript'});
-      console.log("Created Blob with witness_calculator.js content:", wcBlob);
-
-      try {
-        await loadScriptFromBlob(wcBlob);
-        const wc = window.wc;
-        console.log("Imported witness_calculator.js, default export:", wc);
-      } catch (err) {
-        console.error('Error loading script:', err);
-      }
-    },
     metamaskLogin() {
-      // console.log(Prover)
-      // let secretSDK = new SecretSDK('')
-      // console.log(secretSDK)
-      // secretSDK.createAccount();
       this.$emit('login-end', 1)
-      // this.$router.push('/dashboard')
     },
   },
-
-  async mounted() {
-  },
-
   created() {
     if (getAlias()) {
       this.login()
@@ -180,7 +108,6 @@ export default {
       this.user = getSigner()
     }
   },
-
 };
 </script>
 <style lang="scss" scoped>
